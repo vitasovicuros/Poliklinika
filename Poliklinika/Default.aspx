35dcb571a56f1e3b35aa20cf17548ae24e9cab79
MilosSarac <SKOLA@DESKTOP-LTCL6EA>
2022-11-10 18:23:52 +0100

    a

--- Poliklinika/Default.aspx
+++ Poliklinika/Default.aspx
@@ -11,20 +11,20 @@
 
     <asp:Label ID="lblPacijent" runat="server" Text="Pacijent:"></asp:Label>
     &nbsp;
-    <asp:DropDownList ID="ddlPacijent" runat="server"></asp:DropDownList>
+    <asp:DropDownList ID="ddlPacijent" runat="server" AutoPostBack="True"></asp:DropDownList>
     <br /><br />
 
     &nbsp;&nbsp;
 
     <asp:Label ID="lblDoktor" runat="server" Text="Doktor:"></asp:Label>
     &nbsp;&nbsp;&nbsp;
-    <asp:DropDownList ID="ddlDoktor" runat="server"></asp:DropDownList>
+    <asp:DropDownList ID="ddlDoktor" runat="server" AutoPostBack="True"></asp:DropDownList>
     <br /><br />
 
     <asp:Button ID="btnPrikaz" runat="server" Text="Prikaz" OnClick="btnPrikaz_Click" Width="94px" />
     <br /><br />
 
-    <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1"></asp:GridView>
-
-    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:EvidencijaPutnikaConnectionString %>" SelectCommand="SELECT * FROM [Doktor]"></asp:SqlDataSource>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"></asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:PoliklinikaConnectionString %>" SelectCommand="SELECT * FROM [Doktor]"></asp:SqlDataSource>
+ 
 </asp:Content>
--- Poliklinika/Default.aspx.cs
+++ Poliklinika/Default.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Poliklinika
 {
@@ -12,8 +13,11 @@ namespace Poliklinika
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            FillDropDownListDoktor();
-            FillDropDownListPacijent();
+            if (!Page.IsPostBack)
+            {
+                FillDropDownListDoktor();
+                FillDropDownListPacijent();
+            }
         }
 
         protected void FillDropDownListDoktor()
@@ -84,7 +88,47 @@ namespace Poliklinika
 
         protected void btnPrikaz_Click(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = Connection.conString;
+            SqlDataReader reader;
+            string SqlSelect = "Select IdDok, BrKnjizice, Datum, Vreme From Termin";
+            if(ddlDoktor.SelectedItem.Text != "Selektujte doktora" && ddlPacijent.SelectedItem.Text != "Selektujte pacijenta")
+            {
+                SqlSelect += "WHERE IdDok=" + ddlDoktor.SelectedItem.Value + "OR BrKnjizice=" + ddlPacijent.SelectedItem.Value;
+            }
+            else if(ddlDoktor.SelectedItem.Text != "Selektujte doktora" && ddlPacijent.SelectedItem.Text == "Selektujte pacijenta")
+            {
+                SqlSelect += "WHERE IdDok=" + ddlDoktor.SelectedItem.Value;
+            }
+            else if(ddlDoktor.SelectedItem.Text == "Selektujte doktora" && ddlPacijent.SelectedItem.Text != "Selektujte pacijenta")
+            {
+                SqlSelect += "WHERE BrKnjizice=" + ddlPacijent.SelectedItem.Value;
+            }
+            
+            SqlCommand cmd = new SqlCommand(SqlSelect, conn);
 
+            using(conn)
+            {
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Columns.Add("ID Doktora");
+                dataTable.Columns.Add("Broj knjizice");
+                dataTable.Columns.Add("Datum");
+                dataTable.Columns.Add("Vreme");
+                while (reader.Read())
+                {
+                    DataRow row = dataTable.NewRow();
+                    row["ID Doktora"] = reader["IdDok"];
+                    row["Broj knjizice"] = reader["BrKnjizice"];
+                    row["Datum"] = reader["Datum"];
+                    row["Vreme"] = reader["Vreme"];
+
+                    dataTable.Rows.Add(row);
+                }
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
         }
     }
 }
